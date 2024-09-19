0dc1108c097fe14ce81e3a9539df389eb2ca8791
Carlos Miguel Ponte <carlos.m.ponte@outlook.com>
2024-09-19 08:33:54 -0500

    Reveiw

--- TestContainers/RabbitmqCont.cs
+++ TestContainers/RabbitmqCont.cs
@@ -14,26 +14,26 @@ namespace CsharpPlaywrith.TestContainers
     {
         private ContConfig _contConfig = new ContConfig();
         //private readonly _factory;
-        private ConnectionFactory _factory;  // Declaramos 'factory' aquí para usarlo en todas las funciones
-        private  IConnection _connection;     // Conexión global que podemos reutilizar
+        private ConnectionFactory _factory; 
+        private  IConnection _connection;     
         private  IModel _channel;
 
 
        
-        public RabbitmqCont()
+        /*public RabbitmqCont()
         {
             
             
             // Inicializamos 'factory' una vez con la información de conexión a RabbitMQ
        
-        }
+        }*/
         public async Task starRabbitmqCont()
         {
             await _contConfig.SetupRabbitMqContainer();
             //string connectionString = _contConfig.RabbitMqConnectionString;
             _factory = new ConnectionFactory { Uri = new Uri(_contConfig.RabbitMqConnectionString) };
 
-            // Creamos la conexión y el canal para usar en todas las funciones
+            // se crea la  conexión y el canal para usar en todas las funciones
             _connection = _factory.CreateConnection();
             _channel = _connection.CreateModel();
         }
@@ -41,16 +41,16 @@ namespace CsharpPlaywrith.TestContainers
         // Función para enviar un mensaje
         public void SendMessage(string message)
         {
-            // Declaramos la cola 'testQueue' (como si fuera un buzón de mensajes)
+            // Declaramos la cola 'testQueue' 
             _channel.QueueDeclare("testQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-            // Convertimos el mensaje a bytes, que es el formato que RabbitMQ entiende
+            // Convertimos el mensaje a bytes
             var body = Encoding.UTF8.GetBytes(message);
 
             // Publicamos el mensaje en la cola 'testQueue'
             _channel.BasicPublish(exchange: "", routingKey: "testQueue", basicProperties: null, body: body);
 
-            // Mostramos en la consola que el mensaje ha sido enviado
+            
             Console.WriteLine($"[x] Sent {message}");
         }
 
@@ -60,10 +60,10 @@ namespace CsharpPlaywrith.TestContainers
             // Declaramos la cola 'testQueue' (si no existe, la creamos)
             _channel.QueueDeclare("testQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-            // Preparamos un "consumidor", que es como un cartero que recibe los mensajes de la cola
+            // Preparamos el consumido,
             var consumer = new EventingBasicConsumer(_channel);
 
-            // Aquí es donde decimos qué hacer cuando llega un mensaje
+            // Aquí le decimos qué hacer cuando llega un mensaje
             consumer.Received += (model, ea) =>
             {
                 // Obtenemos el cuerpo del mensaje (en bytes) y lo convertimos en texto
